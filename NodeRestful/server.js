09db522db274185fb2fe9a2957cfb5110ce92376
Hai Do truong <haidt@localhost.localdomain>
2017-12-15 17:31:27 +0700

    login get service

--- NodeRestful/server.js
+++ NodeRestful/server.js
@@ -12,11 +12,8 @@ const connection = MySQL.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'haidt123',
-    database: 'database_name'
+    database: 'sateco'
 });
-
-
-
 
 server.connection({
     host: 'localhost',
@@ -32,18 +29,39 @@ server.route({
     }
 });
 
+// Login the route
+server.route({
+    method: 'POST',
+    path: '/login',
+    handler: function (request, reply) {
+        
+        const username = request.payload.username;
+        const password = request.payload.password;
+
+        connection.query('SELECT * FROM user WHERE username = "'+ username +'"', function (error, results, fields) {
+            if (error) throw error;
+            for(let item in results){
+                
+                if(Bcrypt.compareSync(password, results[item].password)){
+                    reply(results[item]);
+                }else{
+                    reply("No");
+                }
+            }
+        });
+    }
+});
+
 // Add the route
 server.route({
     method: 'GET',
-    path: '/users',
+    path: '/user',
     handler: function (request, reply) {
-
-        connection.query('SELECT uid, username, password FROM users', function (error, results, fields) {
+        connection.query('SELECT userid, username, password, user_role FROM user', function (error, results, fields) {
             if (error) throw error;
             console.log(results);
             reply(results);
         });
-
     }
 });
 
@@ -53,7 +71,7 @@ server.route({
     handler: function (request, reply) {
         const uid = request.params.uid;
 
-        connection.query('SELECT uid, username, email FROM users WHERE uid = "' + uid + '"', function (error, results, fields) {
+        connection.query('SELECT userid, username FROM user WHERE userid = "' + uid + '"', function (error, results, fields) {
             if (error) throw error;
             console.log(results);
             reply(results);
@@ -76,7 +94,6 @@ server.route({
     handler: function (request, reply) {
 
         const username = request.payload.username;
-        const email = request.payload.email;
         const password = request.payload.password;
 
         var salt = Bcrypt.genSaltSync();
@@ -84,7 +101,7 @@ server.route({
      
         var orgPassword = Bcrypt.compareSync(password, encryptedPassword);
 
-        connection.query('INSERT INTO users (username,email,password) VALUES ("' + username + '","' + email + '","' + encryptedPassword + '")', function (error, results, fields) {
+        connection.query('INSERT INTO user (username,password,user_role) VALUES ("' + username + '","' + encryptedPassword + '","' + 2 + '")', function (error, results, fields) {
             if (error) throw error;
             console.log(results);
             reply(results);
@@ -95,8 +112,7 @@ server.route({
         validate: {
             payload: {
                 username: Joi.string().alphanum().min(3).max(30).required(),
-                email: Joi.string().email(),
-                password: Joi.string().regex(/^[a-zA-Z0-9]{8,30}$/)
+                password: Joi.string().regex(/^[a-zA-Z0-9]{6,30}$/)
             }
         }
 
@@ -104,88 +120,88 @@ server.route({
 });
 
 
-server.route({
-    method: 'POST',
-    path: '/sendMessage',
-    handler: function (request, reply) {
+// server.route({
+//     method: 'POST',
+//     path: '/sendMessage',
+//     handler: function (request, reply) {
 
-        const uid = request.payload.uid;
-        const message = request.payload.message;
+//         const uid = request.payload.uid;
+//         const message = request.payload.message;
        
-        connection.query('INSERT INTO messages (message,uid_fk) VALUES ("' + message + '","' + uid + '")', function (error, results, fields) {
-            if (error) throw error;
-            console.log(results);
-            reply(results);
-        });
+//         connection.query('INSERT INTO messages (message,uid_fk) VALUES ("' + message + '","' + uid + '")', function (error, results, fields) {
+//             if (error) throw error;
+//             console.log(results);
+//             reply(results);
+//         });
 
-    },
-    config: {
-        validate: {
-            payload: {
-                uid: Joi.number().integer(),
-                message: [Joi.string(), Joi.number()]
-            }
-        }
+//     },
+//     config: {
+//         validate: {
+//             payload: {
+//                 uid: Joi.number().integer(),
+//                 message: [Joi.string(), Joi.number()]
+//             }
+//         }
 
-    }
-});
+//     }
+// });
 
-server.route({
-    method: 'POST',
-    path: '/messages',
+// server.route({
+//     method: 'POST',
+//     path: '/messages',
 
-    handler: function (request, reply) {
+//     handler: function (request, reply) {
 
-        const uid = request.payload.uid;
-        console.log(uid);
+//         const uid = request.payload.uid;
+//         console.log(uid);
 
-        connection.query('SELECT * FROM messages WHERE uid_fk = "' + uid + '"', function (error, results, fields) {
-            if (error) throw error;
-            console.log(results);
-            reply(results);
-        });
+//         connection.query('SELECT * FROM messages WHERE uid_fk = "' + uid + '"', function (error, results, fields) {
+//             if (error) throw error;
+//             console.log(results);
+//             reply(results);
+//         });
 
-    },
-    config: {
-        validate: {
-            payload: {
-                uid: Joi.number().integer()
-            }
-        }
+//     },
+//     config: {
+//         validate: {
+//             payload: {
+//                 uid: Joi.number().integer()
+//             }
+//         }
 
-    }
-});
+//     }
+// });
 
-server.route({
-    method: 'DELETE',
-    path: '/message/{uid}/{mid}',
-    handler: function (request, reply) {
-        const uid = request.params.uid;
-        const mid = request.params.mid;
+// server.route({
+//     method: 'DELETE',
+//     path: '/message/{uid}/{mid}',
+//     handler: function (request, reply) {
+//         const uid = request.params.uid;
+//         const mid = request.params.mid;
 
-        console.log(uid + "---" + mid);
+//         console.log(uid + "---" + mid);
 
-        connection.query('DELETE FROM messages WHERE uid_fk = "' + uid + '"AND mid = "' + mid + '"', function (error, result, fields) {
-            if (error) throw error;
+//         connection.query('DELETE FROM messages WHERE uid_fk = "' + uid + '"AND mid = "' + mid + '"', function (error, result, fields) {
+//             if (error) throw error;
 
-            if (result.affectedRows) {
-                reply(true);
-            } else {
-                reply(false);
-            }
+//             if (result.affectedRows) {
+//                 reply(true);
+//             } else {
+//                 reply(false);
+//             }
 
-        });
-    },
-    config: {
-        validate: {
-            params: {
-                uid: Joi.number().integer(),
-                mid: Joi.number().integer()
-            }
-        }
+//         });
+//     },
+//     config: {
+//         validate: {
+//             params: {
+//                 uid: Joi.number().integer(),
+//                 mid: Joi.number().integer()
+//             }
+//         }
 
-    }
-});
+//     }
+// });
 
 
 // Start the server
